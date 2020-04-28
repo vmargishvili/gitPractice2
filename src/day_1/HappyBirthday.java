4b0283099fa9d6dc86da2430d28bb29c121ba17e
vmargishvili <64220403+vmargishvili@users.noreply.github.com>
2020-04-28 12:53:37 -0700

    Update HappyBirthday.java

--- src/day_1/HappyBirthday.java
+++ src/day_1/HappyBirthday.java
@@ -4,5 +4,8 @@ public class HappyBirthday {
 
     public static void main(String[] args) {
         System.out.println("Happy Birthday B18");
+        
+        
+        System.out.println("Happy Birthday again, this line edit from GitHub")
     }
 }
