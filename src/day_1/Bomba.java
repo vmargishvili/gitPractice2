2f440cfbc84c3ade4948f5dc494db757d08de335
vmargishvili <superyear2020@gmail.com>
2020-04-28 12:39:15 -0700

    add HappyBirthday.java

--- src/day_1/Bomba.java
+++
@@ -1,9 +0,0 @@
-package day_1;
-
-public class Bomba {
-
-    public static void main(String[] args) {
-
-        //I hope everything is GOOD.
-    }
-}
---
+++ src/day_1/addCommand.java
@@ -0,0 +1,10 @@
+package day_1;
+
+public class addCommand {
+
+    public static void main(String[] args) {
+
+        // i want to add only this class to staging area
+
+    }
+}
